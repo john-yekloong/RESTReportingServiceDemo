ed7a8354eb5b970c37756cb30a72227278a4a20b
john-yekloong <john.lim@etrdigital.com>
2022-07-18 11:04:27 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using System;
+using Telerik.Reporting.Cache.File;
+using Telerik.Reporting.Services;
+using Telerik.WebReportDesigner.Services;
+
+EnableTracing();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+builder.Services.AddControllers();
+builder.Services.AddRazorPages()
+    .AddNewtonsoftJson();
+
+var reportsPath = System.IO.Path.Combine(builder.Environment.ContentRootPath, "Reports");
+
+// Configure dependencies for ReportsController.
+builder.Services.TryAddSingleton<IReportServiceConfiguration>(sp =>
+    new ReportServiceConfiguration
+    {
+        // The default ReportingEngineConfiguration will be initialized from appsettings.json or appsettings.{EnvironmentName}.json:
+        ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
+
+        // In case the ReportingEngineConfiguration needs to be loaded from a specific configuration file, use the approach below:
+        //ReportingEngineConfiguration = ResolveSpecificReportingConfiguration(sp.GetService<IWebHostEnvironment>()),
+        HostAppId = "Net6RestServiceWithCors",
+        Storage = new FileStorage(),
+        ReportSourceResolver = new TypeReportSourceResolver()
+            .AddFallbackResolver(new UriReportSourceResolver(reportsPath))
+    });
+
+// Configure dependencies for ReportDesignerController.
+builder.Services.TryAddSingleton<IReportDesignerServiceConfiguration>(sp => new ReportDesignerServiceConfiguration
+{
+    DefinitionStorage = new FileDefinitionStorage(
+        reportsPath),
+    SettingsStorage = new FileSettingsStorage(
+        System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Telerik Reporting")),
+    ResourceStorage = new ResourceStorage(
+        System.IO.Path.Combine(reportsPath, "Resources"))
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
+
+/// <summary>
+/// Uncomment the lines to enable tracing in the current application.
+/// The trace log will be persisted in a file named log.txt in the application root directory.
+/// </summary>
+static void EnableTracing()
+{
+    // System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(File.CreateText("log.txt")));
+    // System.Diagnostics.Trace.AutoFlush = true;
+}
